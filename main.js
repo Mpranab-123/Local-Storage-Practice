abd0fc0babec72e8531561b6c28387b2ba64447a
Mpranab-123 <pranabk.mishra05@gmail.com>
2023-01-10 01:16:08 +0530

    Local Storage practice

---
+++ main.js
@@ -0,0 +1,10 @@
+function saveToLocalStorage(event){
+    event.preventDefault();
+    const name=event.target.name.value;
+    const email=event.target.email.value;
+    const phno=event.target.phoneNumber.value;
+
+    localStorage.setItem('name', name);
+    localStorage.setItem('email', email);
+    localStorage.setItem('phoneNumber',phno);
+}
